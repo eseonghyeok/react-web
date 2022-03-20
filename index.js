fa177e6cf8f52e5a4b32113fd0de8cbf169e1982
이성혁 <lsh4270mom@naver.com>
2022-03-21 04:13:19 +0900

    회원가입 기능

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express'); //다운받은 express모듈 가져옴
 const app = express(); // 새로운 express app을 만듬
 const port = 5000; //5000번 포트를 백서버로
+//바디파서로 client에서 정보 받아올수있음
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
 
+//application/x-www-form-urlencoded 데이터를 분석해서 가져옴.
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json 데이터 분석해서 가져옴
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://lsh:dltjdgur123@cluster0.5jaze.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -11,6 +18,21 @@ mongoose.connect('mongodb+srv://lsh:dltjdgur123@cluster0.5jaze.mongodb.net/myFir
 
 app.get('/', (req, res) => res.send('hello world')); // '/'디렉토리에 오면 출력
 
+
+app.post('/register', (req, res) => {//회원가입을 위한 router
+    //회원가입 할때 필요한 정보들을 client에서 가져오면
+    //그것들을 DB에 넣어줌
+
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+})
 app.listen(port, () => {
 console.log(`Example app listening on port ${port}!`)
 });//포트할당 번호에서 앱을 실행
